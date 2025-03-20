7502399e2e9ed3af8c71e78ebd499d909b5429e9
SangwookBaek <mindustrylab@gmail.com>
2025-03-20 09:45:43 +0000

    update items and ranking for top k

--- app.py
+++ app.py
@@ -5,20 +5,22 @@ from embedder import  Embedding_Model
 from gemini_utils import translate
 import os 
 
-from torch.nn.functional import cosine_similarity
-from torch import argmax as argmax
+from torch import matmul as matmul
+from torch import argsort as argsort
 from torch import load as load
+from torch import Tensor as torch_Tensor
 
-def fetch_products(query_embedding):
-    cos_sim = cosine_similarity(
-        image_embeddings,
-        query_embedding.unsqueeze(0).expand_as(image_embeddings),
-        dim=1
-    )
-    best_idx = argmax(cos_sim).item()
 
-    return best_idx
+def fetch_ranking(query_embedding, image_embeddings,k=3):
+    """
+    주어진 query_embedding에 대해, 이미지 임베딩들과의 dot product 계산하고 (normalized 되어있음)
+    유사도 기준 내림차순으로 정렬된 인덱스 리스트를 반환합니다.
+    -> top - K 반환
+    """
 
+    dot_sim = matmul(image_embeddings, query_embedding.unsqueeze(1)).squeeze(1)
+    ranking = argsort(dot_sim, descending=True)
+    return ranking[:k]
 
 @st.cache_data
 def prepare():
@@ -26,7 +28,7 @@ def prepare():
     '''
     Just for Demo
     '''
-    with open('./items_with_detail_updated.csv', 'r', newline='', encoding='utf-8') as csvfile:
+    with open('./items.csv', 'r', newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
         item_list = list(reader)
     image_embeddings= load('image_embeddings.pt')
@@ -42,28 +44,31 @@ st.title("🛍️ 제품 추천 웹앱")
 # 사용자 입력 받기
 query = st.text_input("검색할 제품 키워드를 입력하세요: ", "")
 
+K = 3
 # 검색 버튼
 if st.button("검색"):
     if query:
         eng_query = translate(query)
         # 제품 정보 가져오기
         print(eng_query)
-        query_embedding=embedder.embed_text(eng_query)
-        best_idx = fetch_products(query_embedding)
-        product = item_list[best_idx]
-        if product:
+        query_embedding = embedder.embed_text(eng_query)
+        rankings_K = fetch_ranking(query_embedding, image_embeddings, K)  # 상위 K개의 인덱스 리스트 반환
+        
+        if isinstance(rankings_K, torch_Tensor):
             st.write("### 검색 결과")
-            
-            # 이미지와 텍스트 영역 비율 설정
-            col1, col2 = st.columns([1, 3])
-            
-            with col1:
-                st.image(product['Image_paths'], width=150)  # 이미지 출력
-                
-            with col2:
-                st.write(f"**[{product['Name']}]({product['Link']})**")
-                st.write(f"💰 가격: {product['Price']}")
-                st.markdown(f"[🔗 구매 링크]({product['Link']})")
+            # ranking 결과에 있는 각 상품을 출력
+            for rank, idx in enumerate(rankings_K, start=1):
+                product = item_list[idx]
+                st.markdown(f"#### 결과 {rank}")
+                # 이미지와 텍스트 영역 비율 설정
+                col1, col2 = st.columns([1, 3])
+                with col1:
+                    st.image(product['Image_path'], width=150)  # 이미지 출력
+                with col2:
+                    st.write(f"**[{product['Name']}]({product['Link']})**")
+                    st.write(f"💰 가격: {product['Price']}")
+                    st.markdown(f"[🔗 구매 링크]({product['Link']})")
+                st.markdown("---")  # 각 결과 사이 구분선
         else:
             st.warning("검색 결과가 없습니다.")
     else:
--- embedder.py
+++ embedder.py
@@ -5,32 +5,37 @@ from PIL import Image
 from typing import List
 
 class Embedding_Model():
-    def __init__(self,model_name = 'Marqo/marqo-fashionCLIP'):
-        self.model_name  = model_name
+    def __init__(self, model_name='Marqo/marqo-fashionCLIP'):
+        self.model_name = model_name
         self.processor = AutoProcessor.from_pretrained(model_name, trust_remote_code=True)
         self.model = AutoModel.from_pretrained(model_name, trust_remote_code=True)
 
-    def embed_images(self,image_paths : List) : 
+    def embed_images(self, image_paths):
         all_images = [Image.open(path).convert("RGB") for path in image_paths]
         # 한 번에 이미지 임베딩 계산 (배치 처리)
         inputs = self.processor(images=all_images, return_tensors="pt", padding=True)
         with no_grad():
-            return self.model.get_image_features(**inputs)
+            return self.model.get_image_features(**inputs,normalize=True)
 
-    def embed_image(self,image_path):
+    def embed_image(self, image_path):
         image = Image.open(image_path).convert("RGB")
         inputs = self.processor(images=image, return_tensors="pt")
         with no_grad():
-            image_features = self.model.get_image_features(**inputs)
+            image_features = self.model.get_image_features(**inputs,normalize=True)
         return image_features.squeeze(0)  # 배치 차원 제거
 
     # 텍스트 임베딩 함수
-    def embed_text(self,text, max_length=77):
-        inputs = self.processor(text=[text], return_tensors="pt", truncation=True, padding='max_length', max_length=max_length)
+    def embed_text(self, text, max_length=77):
+        inputs = self.processor(
+            text=[text], 
+            return_tensors="pt", 
+            truncation=True, 
+            padding='max_length', 
+            max_length=max_length
+        )
         with no_grad():
-            text_features = self.model.get_text_features(**inputs)
+            text_features = self.model.get_text_features(**inputs,normalize=True)
         return text_features.squeeze(0)
-
 if __name__ == '__main__':
     model = Embedding_Model()
     model.embed_text('hi')
